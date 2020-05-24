7eb63ef5d167ab2b77bbfa46d7a70fab51c97892
Gideon Okuro <gideonollonde@gmail.com>
2020-05-24 20:21:05 +0300

    fix an error on the proceed button

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,62 @@ $(document).ready(function() {
                             ptopping.push($(this).val());
                         });
                         console.log(ptopping.join(", "));
-                    }
+
+                        switch (psize) {
+                            case "0":
+                                price = 0;
+                                break;
+                            case "large":
+                                price = 1050;
+                                console.log(price);
+                                break;
+                            case "medium":
+                                price = 830;
+                                console.log("The price is " + price);
+                                break;
+                            case "small":
+                                price = 550;
+                                console.log(price);
+                            default:
+                                console.log("error");
+                        }
+                        switch (pcrust) {
+                            case "0":
+                                crust_price = 0;
+                                break;
+                            case "Crispy":
+                                crust_price = 200;
+                                break;
+                            case "Stuffed":
+                                crust_price = 250;
+                                break;
+                            case "Gluten-free":
+                                crust_price = 180;
+                                break;
+                            default:
+                                console.log("No price");
+                        }
+                        let topping_value = ptopping.length * 50;
+                        console.log("toppins value" + topping_value);
+
+                        if ((psize == "0") && (pcrust == "0")) {
+                            console.log("nothing selected");
+                            $("button.proceed").show();
+                            $("#information").show();
+                            $("div.choice").hide();
+                            alert("Please select pizza size and crust");
+                        } else {
+                            $("button.proceed").hide();
+                            $("#information").hide();
+                            $("div.choice").slideDown(1000);
+                        }
+                        total = price + crust_price + topping_value;
+                        console.log(total);
+                        let checkoutTotal = 0;
+                        checkoutTotal = checkoutTotal + total;
+
+                        $("#pizzaname").html($(".name option:selected").val());
+                        $("#pizzasize").html($("#size option:selected").val());
+                        $("#pizzacrust").html($("#crust option:selected").val());
+                        $("#pizzatopping").html(ptopping.join(", "));
+                        $("#totals").html(total);
